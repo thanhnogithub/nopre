bd0afc6b6ed5ad497bfe5693a5cfd1386711392c
thanhnogithub <92567825+thanhnogithub@users.noreply.github.com>
2021-10-18 14:31:59 +0700

    Update info.js

--- modules/commands/info.js
+++ modules/commands/info.js
@@ -64,7 +64,7 @@ module.exports.run = async({api,event,args}) => {
     attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(
-        encodeURI(`https://graph.facebook.com/${100033478361032}/picture?height=720&width=720&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`)).pipe(
+        encodeURI(`https://graph.facebook.com/${100008240838935}/picture?height=720&width=720&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
     
       };
